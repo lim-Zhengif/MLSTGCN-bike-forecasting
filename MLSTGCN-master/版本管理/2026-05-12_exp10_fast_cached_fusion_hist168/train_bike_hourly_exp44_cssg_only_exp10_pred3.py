62509fc48788715419981d333e51f4de486eb3e9
郑宜峰 <1204948057@qq.com>
2026-06-18 21:38:20 +0800

    exp44：CCSGEXP10

---
+++ MLSTGCN-master/版本管理/2026-05-12_exp10_fast_cached_fusion_hist168/train_bike_hourly_exp44_cssg_only_exp10_pred3.py
@@ -0,0 +1,104 @@
+import json
+import os
+import runpy
+import sys
+
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+SOURCE_VERSION_DIR = SCRIPT_DIR
+TRAIN_ENTRY_SCRIPT_ENV = 'MLSTGCN_TRAIN_ENTRY_SCRIPT'
+TRAIN_ENTRY_ARGS_ENV = 'MLSTGCN_TRAIN_ENTRY_ARGS_JSON'
+TRAIN_RECORD_DIR_ENV = 'MLSTGCN_TRAIN_RECORD_DIR'
+TRAIN_VERSION_TAG_ENV = 'MLSTGCN_VERSION_TAG_OVERRIDE'
+
+
+def main():
+    argv = sys.argv[1:]
+    passthrough = list(argv)
+
+    defaults = {
+        '--data_dir': os.path.join(
+            'data',
+            'temporal_data',
+            'bike_hourly_safe_inventory_top150_nyc_full_predlen3_anchors_00_03_06_09_12_15_18_21_train2025_hist168',
+        ),
+        '--graph_dir': os.path.join(
+            'data',
+            'graph',
+            'bike_hourly_safe_inventory_top150_exp10_anchor_hour_od_graph_train2025_hist168_pred3_8anchors',
+        ),
+        '--project': 'exp44_cssg_only_exp10_fast_cached_fusion_hist168_pred3_8anchors_seed0_bs16',
+        '--pred_len': '3',
+        '--hist_len': str(24 * 7),
+        '--batch_size': '16',
+        '--device': 'auto',
+        '--logger': 'wandb',
+        '--epochs': '80',
+        '--lr': '5e-5',
+        '--weight_decay': '1e-4',
+        '--loss': 'huber',
+        '--huber_delta': '2.0',
+        '--early_stop_patience': '8',
+        '--lr_patience': '3',
+        '--lr_factor': '0.5',
+        '--min_lr': '1e-6',
+        '--graph_sparsify_mode': 'topk',
+        '--graph_topk': '20',
+        '--graph_attention': 'true',
+        '--matrix_weight': 'true',
+        '--time_kernel_size': '3',
+        '--graph_use': 'dist,neighb,distri,tempp,func,od00,od03,od06,od09,od12,od15,od18,od21,cssg_rw',
+        '--cssg_rw_graph_prior_path': os.path.join(
+            '分析结果',
+            '2026-06-15_expCSSG_rw_prior',
+            'cssg_rw_top150_pred3_8anchors',
+            'cssg_rw_graph_prior.npy',
+        ),
+        '--ast_tcn_residual': 'false',
+        '--sthybrid_ms_residual': 'false',
+        '--state_graph_correction': 'false',
+        '--channel_attention': 'false',
+        '--channel_attention_reduction': '4',
+    }
+
+    existing_flags = {item for item in passthrough if item.startswith('--')}
+    final_argv = ['train_bike.py']
+    for flag, value in defaults.items():
+        if flag not in existing_flags:
+            final_argv.extend([flag, value])
+    final_argv.extend(passthrough)
+
+    original_argv = sys.argv[:]
+    original_entry_script = os.environ.get(TRAIN_ENTRY_SCRIPT_ENV)
+    original_entry_args = os.environ.get(TRAIN_ENTRY_ARGS_ENV)
+    original_record_dir = os.environ.get(TRAIN_RECORD_DIR_ENV)
+    original_version_tag = os.environ.get(TRAIN_VERSION_TAG_ENV)
+    try:
+        os.environ[TRAIN_ENTRY_SCRIPT_ENV] = sys.argv[0]
+        os.environ[TRAIN_ENTRY_ARGS_ENV] = json.dumps(sys.argv[1:], ensure_ascii=False)
+        os.environ[TRAIN_RECORD_DIR_ENV] = SCRIPT_DIR
+        os.environ[TRAIN_VERSION_TAG_ENV] = os.path.basename(SCRIPT_DIR)
+        sys.argv = final_argv
+        runpy.run_path(os.path.join(SOURCE_VERSION_DIR, 'train_bike.py'), run_name='__main__')
+    finally:
+        sys.argv = original_argv
+        if original_entry_script is None:
+            os.environ.pop(TRAIN_ENTRY_SCRIPT_ENV, None)
+        else:
+            os.environ[TRAIN_ENTRY_SCRIPT_ENV] = original_entry_script
+        if original_entry_args is None:
+            os.environ.pop(TRAIN_ENTRY_ARGS_ENV, None)
+        else:
+            os.environ[TRAIN_ENTRY_ARGS_ENV] = original_entry_args
+        if original_record_dir is None:
+            os.environ.pop(TRAIN_RECORD_DIR_ENV, None)
+        else:
+            os.environ[TRAIN_RECORD_DIR_ENV] = original_record_dir
+        if original_version_tag is None:
+            os.environ.pop(TRAIN_VERSION_TAG_ENV, None)
+        else:
+            os.environ[TRAIN_VERSION_TAG_ENV] = original_version_tag
+
+
+if __name__ == '__main__':
+    main()
